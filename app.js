9a8ade5e7ae01e0806cb7d19fd7c7595e26b7094
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-18 19:02:44 -0400

    move server files to server

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const http = require('http');
+const firebase = require('firebase');
+
+const config = {
+  apiKey: 'AIzaSyBxF-kdsFqqxEQ83HnoD_-eDpcdPeHFyFQ',
+  authDomain: 'wake-county-mutual-aid.firebaseapp.com',
+  databaseURL: 'https://wake-county-mutual-aid.firebaseio.com',
+  projectId: 'wake-county-mutual-aid',
+  storageBucket: 'wake-county-mutual-aid.appspot.com',
+  messagingSenderId: '363954067119',
+};
+firebase.initializeApp(config);
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'server/public')));
 
 app.get('/', (req, res) => {
   res.render('index');
