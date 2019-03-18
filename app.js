53f429d6f2dd26d33eb6cf746d2eb1c3f5eac63d
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-18 18:34:36 -0400

    set up a basic site

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+#!/usr/bin/env node
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,23 +15,69 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use('*', (req, res) => {
+  res.status(404).send('404');
 });
 
-module.exports = app;
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+
+function normalizePort(val) {
+  const portInt = parseInt(val, 10);
+
+  if (isNaN(portInt)) {
+    // named pipe
+    return val;
+  }
+
+  if (portInt >= 0) {
+    // port number
+    return portInt;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+const server = http.createServer(app);
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+}
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
