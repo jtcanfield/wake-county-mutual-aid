2d0367d509af2fd65a0d75cb81a00db66b1d3c5c
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-18 20:30:41 -0400

    add vue project

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>Wake County Mutual Aid Collective</h1>
+    <p>
+      Coming Soon
+    </p>
+    <h3>Links</h3>
+    <ul>
+      <li><a href="https://www.facebook.com/Wake-County-Mutual-Aid-Collective-390108055141422/" target="_blank" rel="noopener">Facebook</a></li>
+      <li><a href="https://twitter.com/WCMutualAid" target="_blank" rel="noopener">Twitter</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
