d4f8dbd9c5d4ac5f79096cccdad76c46850c2dd9
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-21 19:45:51 -0400

    update function to be functional

--- functions/email/index.js
+++ functions/email/index.js
@@ -1,32 +1,24 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+require('dotenv').config();
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+const mailgun = require('mailgun-js')({ apiKey: process.env.API_KEY, domain: process.env.DOMAIN_NAME });
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
+exports.sendMail = functions.https.onRequest((body) => {
+  const text = `
+  Email: ${body.email}
+  Phone: ${body.phone}
+  Message: ${body.message}
+  `;
+  const data = {
+    from: body.email || 'wakecountymutualaidcollective@website.com',
+    to: 'wakecountymutualaidcollective@gmail.com',
+    // cc: process.env.EMAIL_2,
+    subject: 'Website Message',
+    text,
+  };
+  return mailgun.messages().send(data, (error, res) => {
+    console.log(body);
   });
 });
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-  .onCreate((snapshot, context) => {
-    // Grab the current value of what was written to the Realtime Database.
-    const original = snapshot.val();
-    console.log('Uppercasing', context.params.pushId, original);
-    const uppercase = original.toUpperCase();
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    return snapshot.ref.parent.child('uppercase').set(uppercase);
-  });
