9b50e7eaf603e0b4062f6956bc8c50789c60d5bf
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-03-29 13:35:45 -0400

    add uglifyjs plugin, and update packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   productionSourceMap: false,
   lintOnSave: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+      minimizer: [new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i,
+        include: /.*?/,
+      })],
+    },
+  },
 };
